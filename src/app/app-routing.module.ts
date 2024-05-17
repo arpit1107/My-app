2fc51ca403b360daa50ba56b2e9ae8bb16f9c094
arpit1107 <arpit1107programming@gmail.com>
2024-05-17 22:14:20 +0530

    header is added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [
-  {path:'Login', component:LoginComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
-import { TestService } from './Services/test.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name : string ='';
-  constructor(test : TestService){
-    this.name = test.printdata();
-  }
-  title = 'my-app';
+ 
   
 }
