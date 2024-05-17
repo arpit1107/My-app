bed3bf14e171117832305eabdf936e9a4200c30b
arpit1107 <arpit1107programming@gmail.com>
2024-05-17 21:32:23 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TestService } from './Services/test.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  name : string ='';
+  constructor(test : TestService){
+    this.name = test.printdata();
+  }
   title = 'my-app';
+  
 }
